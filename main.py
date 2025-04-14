1965bd9a13fb5746efc49c35b31468c832217da8
kartikunnithan <kartikunnithan738@gmail.com>
2025-04-14 15:00:48 +0400

    12

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import re, random
 from colorama import Fore, init
 init(autoreset=True)
 destinations = {
-    "beaches": ["Bali", "Maldives", "Phuket"],
-    "mountains": ["Swiss Alps", "Rocky Mountains", "Himalayas"],
-    "cities": ["Tokyo", "Paris", "New York"]
+    "Weather": ["Summer ", "Winter", "Spring"],
+    " Time in cities": ["Tokyo", "Paris", "New York"]
 }
 jokes = [
     "Why don't programmers like nature? Too many bugs!",
